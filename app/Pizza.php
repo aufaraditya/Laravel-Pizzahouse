c327990a64cb7dd185704f45fd419da458c57a44
Aufar <adityaaufar@gmail.com>
2021-07-12 19:18:49 +0700

    t

--- app/Pizza.php
+++ app/Pizza.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Pizza extends Model  // The model connects to the table which have the plural form of Model name
 {
     protected $casts = [
-        'toppings' => 'array' //Switch yyyyyyyaaayyyyyybetween Json (as per data type in the database) with Array( as per data from the input)
+        'toppings' => 'array' //Switch between Json (as per data type in the database) with Array( as per data from the input)
 
     ];
 }
